9eaf7c85647330dfee6e5d69172af3facbce30cf
RipplerES <Richard.doucet@ripplerES.com>
2018-05-11 14:43:59 -0700

    test

---
+++ tests/RangeTests.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Linq;
+using api.Controllers;
+using Xunit;
+
+namespace tests
+{
+    public class RangeTests
+    {
+        [Fact]
+        public void CountShouldControlNumberOfResults()
+        {
+            var range = new Range { Count = 3 };
+            var generate = range.Of(() => "");
+            Assert.Equal(3, generate.Count());
+        }
+
+        [Fact]
+        public void SortShouldOrderResults() 
+        {
+            var range = new Range { Count = 3, Sort = 2 };
+            var calues = new { "a", "c", "b" };
+            var counter = 0;
+            var generated = range.Of(() => values[counter++]);
+
+            Assert.Equal(new[] {"a", "b", "c"}, generated.ToArray());
+        }
+    }
+}
